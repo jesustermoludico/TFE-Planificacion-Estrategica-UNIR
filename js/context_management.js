3984cb9b38d2e255633f9546197082263678e971
Jesús Villarroya Lancis <jesustermoludico@gmail.com>
2026-07-08 13:15:33 +0200

    Add project documentation and context description

--- js/context_management.js
+++ js/context_management.js
@@ -1,4 +1,16 @@
 /**
+ * Planificación Estratégica Docente (TFE - UNIR)
+ * ------------------------------------------------------------------
+ * Proyecto desarrollado como parte del Trabajo de Fin de Estudio (TFE)
+ * del Grado en Ingeniería Informática de la Universidad Internacional
+ * de La Rioja (UNIR).
+ *
+ * Autor: Jesús Villarroya Lancis
+ * Licencia: MIT (ver archivo LICENSE)
+ * Repositorio: https://github.com/jesustermoludico/TFEPlanificacion-Estrategica-UNIR
+ */
+
+/**
  * Context Management System - Versión Simplificada
  * Solo maneja requisitos 1.1.1 y 1.1.2: ciclos de FP por grado y cambio de comunidad
  */
