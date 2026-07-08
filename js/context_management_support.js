acf814da4d714a54417692b06795a96b8a7ce906
Jesús Villarroya Lancis <jesustermoludico@gmail.com>
2026-07-08 13:15:45 +0200

    Update context_management_support.js header comments
    Added project information and license details.

--- js/context_management_support.js
+++ js/context_management_support.js
@@ -1,4 +1,16 @@
 /**
+ * Planificación Estratégica Docente (TFE - UNIR)
+ * ------------------------------------------------------------------
+ * Proyecto desarrollado como parte del Trabajo de Fin de Estudio (TFE)
+ * del Grado en Ingeniería Informática de la Universidad Internacional
+ * de La Rioja (UNIR).
+ *
+ * Autor: Jesús Villarroya Lancis
+ * Licencia: MIT (ver archivo LICENSE)
+ * Repositorio: https://github.com/jesustermoludico/TFEPlanificacion-Estrategica-UNIR
+ */
+
+/**
  * Context Management Support - Soporte adicional para funcionalidades de Educaci�n Secundaria
  * Trabaja en conjunto con context_management.js
  */
